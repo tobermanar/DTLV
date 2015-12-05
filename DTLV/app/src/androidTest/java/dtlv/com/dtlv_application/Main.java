65dab10734a0508e100d83eb84f6827afbae6b32
tobermanar <tobermanar@hotmail.fr>
2015-12-05 12:58:53 -0500

    Création des ressources de base
    Création des ressources tests_text en francais et anglais. Création des
    ressources tests_images en francais et anglais.

---
+++ DTLV/app/src/androidTest/java/dtlv/com/dtlv_application/Main.java
@@ -0,0 +1,8 @@
+package dtlv.com.dtlv_application;
+
+/**
+ * Created by Utilisateur on 2015-12-05.
+ */
+public class Main {
+
+}
---
+++ DTLV/app/src/androidTest/java/dtlv/com/dtlv_application/Menu.java
@@ -0,0 +1,7 @@
+package dtlv.com.dtlv_application;
+
+/**
+ * Created by Utilisateur on 2015-12-05.
+ */
+public class Menu {
+}
