25e2b17584652d77214e9756003f518d57b945a0
tobermanar <tobermanar@hotmail.fr>
2015-12-05 23:33:23 -0500

    Correction des ressources
    Correction des ressources pour permettre au debug de fonctionner. Erreur
    principale : Présence de ' sans \ avant.

--- DTLV/app/src/androidTest/java/dtlv/com/dtlv_application/Test2.java
+++ DTLV/app/src/androidTest/java/dtlv/com/dtlv_application/Test2.java
@@ -1,9 +1,18 @@
 package dtlv.com.dtlv_application;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 
 /**
  * Created by Utilisateur on 2015-12-05.
  */
 public class Test2 extends Activity {
+    /*MediaPlayer media = MediaPlayer.create(getContext(), R.raw.file);
+// public static MediaPlayer create (Context context, Uri uri)
+    media = MediaPlayer.create(getContext(), Uri.parse("file://android_asset/fichier.mp4");
+    media = MediaPlayer.create(getContext(), Uri.parse("file://sdcard/music/fichier.mp3");
+    void setDataSource(String path);
+    void prepare();
+      // juste un test à suppr ou à utiliser si besoin */
+
 }
