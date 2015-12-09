59daf6a7fd5aefb5dac872234095b3a727075ced
tobermanar <tobermanar@hotmail.fr>
2015-12-09 13:52:57 -0500

    Modification classe config et test1, modification layout config et test1
    Pas de bug conservés. Test 1 il faut finir l'affichage en grand d'une
    image. Config, il faut étendre les paramètres de luminosité à toute
    l'application.

--- DTLV/app/src/main/java/dtlv/com/dtlv_application/Config.java
+++ DTLV/app/src/main/java/dtlv/com/dtlv_application/Config.java
@@ -15,33 +15,34 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 public class Config extends Activity {
 
         private ImageButton bconfig_next = null;
-        private SeekBar sbrightness;
-
+        private SeekBar sbrightness = null;
+        float brightnessSetting = 0;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.config);
             bconfig_next = (ImageButton) findViewById(R.id.config_next);
             sbrightness = (SeekBar) findViewById(R.id.brightness);
+
             sbrightness.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar sbrightness, int progress, boolean fromUser) {
                     WindowManager.LayoutParams lp = getWindow().getAttributes();
-                    lp.screenBrightness = (progress / 100); // 0.0 - 1.0
+                    brightnessSetting = (float)progress/100;
+                    lp.screenBrightness = brightnessSetting; // 0.0 - 1.0
                     getWindow().setAttributes(lp);
                 }
                 @Override
                 public void onStartTrackingTouch(SeekBar seekBar) {
-
                 }
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-
                 }
             });
                 bconfig_next.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
                         // Au click sur le bouton, on passe au test suivant
                         Intent itest1 = new Intent(Config.this, Test1.class);
                         startActivity(itest1);
