a48c98a34a3202c96cdd1b11166df0ad0ef0f543
Nourry <nourryantoine2@live.fr>
2015-12-12 22:03:43 -0500

    Remise en forme du config.xml Preparation du tutorial.xml

--- DTLV/app/src/main/java/dtlv/com/dtlv_application/Tutorial.java
+++ DTLV/app/src/main/java/dtlv/com/dtlv_application/Tutorial.java
@@ -11,6 +11,11 @@ import android.widget.RadioButton;
 public class Tutorial extends Activity{
 
 
+    /**
+     * Idee: Pour l'appuis sur un bouton, changer l'image correspondante dans le "tuto_scroll" qui est un scroll_view
+     * Soit changer la source de l'image dans le Image view? Ou alors carrement switcher entre les images view?
+     */
+
     private RadioButton r1 = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
